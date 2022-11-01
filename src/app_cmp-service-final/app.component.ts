ca5b80e758b92a261fbf379d64f84dd09f606cb5
Alby2901 <a.azzola@scriptanet.it>
2022-11-01 21:07:04 +0100

    20221101-04

---
+++ src/app_cmp-service-final/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { UsersService } from './service/users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // activeUsers = ['Max', 'Anna'];
+  // inactiveUsers = ['Chris', 'Manu'];
+
+  activeUsers = this.usersService.activeUsers;
+  inactiveUsers = this.usersService.inactiveUsers;
+
+constructor(private usersService: UsersService){}
+
+  onSetToInactive(id: number) {
+    // this.inactiveUsers.push(this.activeUsers[id]);
+    // this.activeUsers.splice(id, 1);
+    this.usersService.setToInactive(id);
+  }
+
+  onSetToActive(id: number) {
+    // this.activeUsers.push(this.inactiveUsers[id]);
+    // this.inactiveUsers.splice(id, 1);
+    this.usersService.setToActive(id);
+
+
+  }
+}
